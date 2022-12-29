37f2fb915f1a18c644b4314448b8bbbbb66a0b57
Hramal <yojade1991@gmail.com>
2022-12-29 14:47:50 +0100

    Mascota.3

--- src/mascota/RatoncitoFiuFiu.java
+++ src/mascota/RatoncitoFiuFiu.java
@@ -10,6 +10,7 @@ public class RatoncitoFiuFiu {
     private byte salud;
     private byte energia;
 
+    private int vivido;
 
 
     public RatoncitoFiuFiu(String nombre, int peso, byte hambre, byte guarro, byte salud, byte energia) {
@@ -22,8 +23,6 @@ public class RatoncitoFiuFiu {
         this.edad = 0;
 
     }
-
-
 
 
     public String estadisticas() {
@@ -49,20 +48,59 @@ public class RatoncitoFiuFiu {
     public boolean estasDormido() {
         return false;
     }
+    public boolean estasFeliz(){
+        return (this.salud > 80 && this.hambre < 3 && this.guarro < 25);
+    }
 
     public boolean estasEnfermo() {
-        return false;
+        return (this.salud <= 25 && this.salud < 1);
+
     }
 
     public boolean estasSucio() {
-        return false;
+        return this.guarro >= 75;
     }
 
     public boolean estasMuerto() {
-        return false;
+        return this.salud == 0;
+
     }
 
-    public void envejecer(int i) {
+    public boolean tienesHambre(){
+        return this.hambre >7;
+    }
+
+    public void envejecer(int segundos) {
+        this.edad = edad + segundos;
+        vivido += segundos;
+        if (vivido >= 1000) {
+            this.hambre = (byte) (hambre + (byte) (vivido * 0.001));
+            if (this.hambre > 10) {
+                this.hambre = 10;
+                tienesHambre();
+                estasEnfermo();
+            }
+            this.salud = (byte) (salud - (byte) (vivido * 0.01));
+            if (this.salud <= 25) {
+                estasEnfermo();
+            }
+            if (this.salud <= 0) {
+                this.salud = 0;
+                estasMuerto();
+            }
+            this.guarro = (byte) (guarro + (byte) (vivido * 0.01));
+            if (this.guarro > 100) {
+                this.guarro = 100;
+                estasSucio();
+            }
+            if (this.energia == 0) {
+                estasDormido();
+            } else {
+                this.energia = (byte) (energia - (byte) (vivido * 0.01));
+
+            }
+            vivido = 0;
+        }
 
     }
 
